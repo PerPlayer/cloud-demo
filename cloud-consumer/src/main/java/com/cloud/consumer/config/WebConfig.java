632448de43e1f35ae12a0e5a3d1ed3812fdd1c1b
PerPlayer <37969628+PerPlayer@users.noreply.github.com>
2019-06-10 16:31:28 +0800

    swagger yaml

---
+++ cloud-consumer/src/main/java/com/cloud/consumer/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.cloud.consumer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/statics/**")
+                .addResourceLocations("classpath:/statics/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH")
+                .maxAge(3600 * 24);
+    }
+}
